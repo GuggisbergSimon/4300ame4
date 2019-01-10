899fca09092ea27f441be0ab2a57dc40df851ce8
Unknown <slyguggi@gmail.com>
2019-01-10 16:30:10 +0100

    added basic Bridge, working, but not breakable. for now

---
+++ Assets/Scripts/Bridge.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bridge : MonoBehaviour
+{
+	[SerializeField] private BreakablePart[] breakableParts;
+
+	[System.Serializable] private struct BreakablePart
+	{
+		public Collider2D collider;
+		public GameObject sprite;
+	}
+}
