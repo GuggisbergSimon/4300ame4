ac12a7d0f94f2c56b17142a8b1fce433bd829901
Simon <slyguggi@gmail.com>
2019-01-11 09:42:09 +0100

    improved arrows behaviour

--- Assets/Scripts/Arrows.cs
+++ Assets/Scripts/Arrows.cs
@@ -8,7 +8,10 @@ public class Arrows : MonoBehaviour
 	[SerializeField] private float timeBeforeShot = 2.0f;
 	[SerializeField] private float speedAim = 2.0f;
 	[SerializeField] private float speedShot = 10.0f;
+	[SerializeField] private GameObject aim=null;
 
+	private SpriteRenderer spriteAimRenderer;
+	private SpriteRenderer mySpriteRenderer;
 	private Rigidbody2D myRigidBody;
 	private bool isShot = false;
 	private float timer = 0.0f;
@@ -18,15 +21,16 @@ public class Arrows : MonoBehaviour
 	{
 		myRigidBody = GetComponent<Rigidbody2D>();
 		myCollider = GetComponent<Collider2D>();
+		spriteAimRenderer = aim.GetComponentInChildren<SpriteRenderer>();
+		mySpriteRenderer = GetComponentInChildren<SpriteRenderer>();
+		//todo check if invoke is really safe to use
 		Invoke("Shoot", timeBeforeShot);
 	}
 
 	private void OnDrawGizmos()
 	{
-		//TODO for debug purposes, to remove later
 		float distance = 30.0f;
 		RaycastHit2D hit = Physics2D.Raycast(transform.position - Vector3.up * 5.0f, Vector3.down, distance);
-		Gizmos.DrawSphere(hit.point, 1.0f);
 		Gizmos.DrawSphere(transform.position - Vector3.up * 5.0f, timeBeforeShot - timer);
 	}
 
@@ -38,15 +42,21 @@ public class Arrows : MonoBehaviour
 			transform.position += Mathf.Sign((GameManager.Instance.Player.transform.position - transform.position).x) *
 								  Vector3.right * speedAim * Time.deltaTime;
 		}
+
+		//Debug.Log(mySpriteRenderer.sprite.rect.height/2);
+		RaycastHit2D hit = Physics2D.Raycast(transform.position - Vector3.up * mySpriteRenderer.sprite.rect.height/2, Vector3.down);
+		aim.transform.position = hit.point;
 	}
 
 	private void Shoot()
 	{
-		Vector2 differenceVector = GameManager.Instance.Player.transform.position - transform.position;
 		isShot = true;
 		myRigidBody.gravityScale = 1.0f;
-		myRigidBody.velocity = differenceVector.normalized * speedShot;
-		transform.up = -differenceVector;
+		myRigidBody.velocity = Vector2.down * speedShot;
+		//to use if we want to have arrows aiming correctly at player, which I found a bit unfair, unless we set a rotationspeed when aiming
+		//Vector2 differenceVector = GameManager.Instance.Player.transform.position - transform.position;
+		//myRigidBody.velocity = differenceVector.normalized * speedShot;
+		//transform.up = -differenceVector;
 	}
 
 	private void OnTriggerEnter2D(Collider2D other)
@@ -56,17 +66,26 @@ public class Arrows : MonoBehaviour
 			GameManager.Instance.Player.Die();
 			Destroy(this.gameObject);
 		}
-		else if (other.CompareTag("Ground"))
+		else if (other.CompareTag("Arrow"))
 		{
-			myCollider.enabled = false;
-			myRigidBody.bodyType = RigidbodyType2D.Static;
+			//do nothing, as expected
 		}
 		else if (other.CompareTag("Shelter"))
 		{
+			CollisionSolidObject(other);
 			StartCoroutine(other.GetComponent<Shelter>().Burn());
-			myCollider.enabled = false;
-			myRigidBody.bodyType = RigidbodyType2D.Static;
-			transform.SetParent(other.gameObject.transform);
 		}
+		else
+		{
+			CollisionSolidObject(other);
+		}
+	}
+
+	private void CollisionSolidObject(Collider2D other)
+	{
+		myCollider.enabled = false;
+		myRigidBody.bodyType = RigidbodyType2D.Static;
+		aim.SetActive(false);
+		transform.SetParent(other.gameObject.transform);
 	}
 }
