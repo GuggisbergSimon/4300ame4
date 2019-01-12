c45f4e95cd61744accfc44ff4da463f88153f2cf
Unknown <lucafloreau@akeonet.com>
2019-01-12 20:14:17 +0100

    2d ik integration + parameter speed

--- Assets/Scripts/Elephant.cs
+++ Assets/Scripts/Elephant.cs
@@ -8,16 +8,31 @@ public class Elephant : MonoBehaviour
     [SerializeField] private float shakeFrequency;
     [SerializeField] private float shakeTime;
 
+    private Animator animator;
+
+    private float speed;
+
+    public float Speed
+    {
+        get => speed;
+        set => speed = value;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        animator.SetFloat("Speed", speed);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Destroy()
+    {
+        Destroy(gameObject);
     }
 
     public IEnumerator Shake()
--- Assets/Scripts/Horse.cs
+++ Assets/Scripts/Horse.cs
@@ -4,23 +4,37 @@ using UnityEngine;
 
 public class Horse : MonoBehaviour
 {
+    private Animator animator;
+
+    private float speed;
+
+    public float Speed
+    {
+        get => speed;
+        set => speed = value;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        animator.SetFloat("Speed", speed);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    public void Destroy()
+    {
+        Destroy(gameObject);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            StartCoroutine( GameManager.Instance.Player.Die());
+            StartCoroutine(GameManager.Instance.Player.Die());
         }
     }
 }
